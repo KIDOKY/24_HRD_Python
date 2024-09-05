43d78a13d6e2d13283a1af950211ee0603d7fc51
KIDOKY <118824913+KIDOKY@users.noreply.github.com>
2024-09-05 09:04:31 +0900

    Add files via upload

---
+++ pythonProject/Example/Ex001.py
@@ -0,0 +1,8 @@
+print(273, 10, "Python")
+print()
+print()
+age = 40
+print("나는", "kidoky", "이다")
+print(f"나는 kidoky이고 나이는 {age}")
+
+a = 1
---
+++ pythonProject/Example/Ex004.py
@@ -0,0 +1,29 @@
+# x:int = int(input("정숫값을 입력: "))
+# if x > 0:
+#     print("양수")
+# else:
+#     print("음수")
+
+# x:int = int(input("점수 입력: "))
+# if x >= 80:
+#     print("합격")
+# else:
+#     print("불합격")
+
+# x:int = int(input("나이 입력: "))
+# if x >= 65:
+#     print("입장료 무료")
+# else:
+#     print("입장료 없으면 말하시오. 누구든 도와주겠소!")
+
+# x:int = int(input("주민번호 앞자리 입력: "))
+# if x == 1:
+#     print("남성")
+# else:
+#     print("여성")
+
+x:int = int(input("비밀번호 입력: "))
+if x == 1234:
+    print("로그인 되었다")
+else:
+    print("비밀번호가 틀렸다")
